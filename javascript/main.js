f0d837d2487d143c27f2930f38a27c98e86fa7b8
Rocio Diaz <rociodiaz91@hotmail.com>
2021-07-15 12:18:56 -0300

    correcion entrega 4 agregué función para hacer un prompt con una suscripción a newsletter

--- javascript/main.js
+++ javascript/main.js
@@ -10,3 +10,24 @@ if ((newsletter === "") || (OnlyDigits.test(newsletter))) {
     alert(`Tu e-mail es ${newsletter}. ¡Te enviaremos ofertas en breve!`);
 }
  */
+
+
+/* CORRECCIÓN ENTREGA CLASE 4 */
+
+OnlyDigits = /^[0-9]+$/;
+
+function ingresoEmail () {
+    let email = prompt("¿Te gustaría inscribirte a nuestro newsletter? ¡Dejanos tu e-mail");
+
+    if ((email === "") || (OnlyDigits.test(email))) {
+        alert("¡Ups! Ingreso erróneo.");
+
+    }else if (email === null) {
+        alert("Newsletter cancelada.");
+
+    }else {
+        alert(`Te has suscrito a nuestro newsletter con el e-mail: ${email}`);
+
+    }
+}
+ingresoEmail();
