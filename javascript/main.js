80d66159039fa4b7265cdf13ea70f11f18257a37
Rocio Diaz <rociodiaz91@hotmail.com>
2021-08-06 10:27:42 -0300

    cambios de prolijidad en js

--- javascript/main.js
+++ javascript/main.js
@@ -31,37 +31,37 @@ tituloSeccionProductos.innerHTML = 'Nuestros Productos Disponibles';
 // PRIMERAS MODIFICACIONES PARA EL CARRITO - EN PROCESO
 //USO DE DOM, EVENTOS, OBJETOS, ARRAY, 
 
-const Clickbutton = [...document.getElementsByClassName('button')];
+const clickBoton = [...document.getElementsByClassName('button')];
 const tbody = document.querySelector('.tbody')
 let carrito = []
 
-Clickbutton.forEach(btn => {
-    btn.addEventListener('click', addToCarritoItem)
+clickBoton.forEach(btn => {
+    btn.addEventListener('click', agregarItemCarrito)
 })
 
-function addToCarritoItem(e){
+function agregarItemCarrito(e){
     const button = e.target
     const item = button.closest('.item')
-    const itemTitle = item.querySelector('.card-title').textContent;
-    const itemPrice = item.querySelector('.precio').textContent
+    const itemTitulo = item.querySelector('.card-title').textContent;
+    const itemPrecio = item.querySelector('.precio').textContent
     const itemImg = item.querySelector('.img-card').src
     
-    const newItem = {
-        title: itemTitle,
-        precio: itemPrice,
+    const nuevoItem = {
+        titulo: itemTitulo,
+        precio: itemPrecio,
         images: itemImg,
         cantidad: 1
     }
 
-    addItemCarrito(newItem)
+    addItemCarrito(nuevoItem)
 
 }
 
-function addItemCarrito(newItem){
-    const inputElemento = tbody.getElementsByClassName('input-element');
+function addItemCarrito(nuevoItem){
+    const inputElemento = tbody.getElementsByClassName('elemento-input');
 
     for(let i =0; i < carrito.length ; i++){
-        if (carrito[i].title === newItem.title.trim()) {
+        if (carrito[i].titulo === nuevoItem.titulo.trim()) {
             carrito[i].cantidad ++;
             const inputValue = inputElemento[i]
             inputValue.value++;
@@ -70,7 +70,7 @@ function addItemCarrito(newItem){
         }
     }
 
-    carrito.push(newItem)
+    carrito.push(nuevoItem)
 
     renderCarrito()
 }
@@ -80,20 +80,20 @@ function renderCarrito(){
     carrito.map(item => {
         const tr = document.createElement('tr')
         tr.classList.add('ItemCarrito')
-        const Content = `
+        const contenido = `
         <th scope="row">1</th>
                 <td class="table__productos">
                     <img src=${item.images} alt="">
-                    <h6 class="title-carrito">${item.title}</h6>
+                    <h6 class="titulo-carrito">${item.titulo}</h6>
                 </td>
                 <td class="table__precio"><p>${item.precio}</p></td>
                 <td class="table__cantidad">
-                    <input type="number" min="1" value=${item.cantidad} class="input-element">
+                    <input type="number" min="1" value=${item.cantidad} class="elemento-input">
                     <button class="delete btn btn-danger">x</button>
                 </td>
         `
 
-        tr.innerHTML = Content;
+        tr.innerHTML = contenido;
         tbody.append(tr)
 
     })
@@ -110,5 +110,5 @@ function carritoTotal(){
         total = total + precio*item.cantidad
     })
 
-    itemCarritoTotal.innerHTML = `Total ${total}`
+    itemCarritoTotal.innerHTML = `Total: $ ${total}`
 }
